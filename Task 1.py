a956177275f025a7b41809a07e0f4e9bf84bf59f
DerekAB <123401143+DerekAB@users.noreply.github.com>
2023-02-03 00:31:45 +0000

    input

--- Task 1.py
+++ Task 1.py
@@ -1,8 +1,19 @@
-#Name:
-#Author:
-#Date Created:
-#Date Last Modified:
+#Name:                  AreaCalc.py
+#Author:                Derek Baker
+#Date Created:          02-02-2023
+#Date Last Modified:    02-02-2023
 #
 #Purpose:
 #
-#
+#The purpose of this program is to ask the user if they want to calculate the area of a circle, rectangle, or triangle.
+
+answer = input("Please enter if you would like to calculate the area of a triangle, circle, or rectangle. Or if you would like to exit: ")
+
+while answer.strip().upper() == "TRIANGLE":
+    
+    
+while answer.strip().upper() == "RECTANGLE":
+    
+    
+while answer.strip().upper() == "CIRCLE":
+    
