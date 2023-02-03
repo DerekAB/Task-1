4b42b5e5a57015dbf8fb15934c5776c759fd9d95
DerekAB <123401143+DerekAB@users.noreply.github.com>
2023-02-03 01:51:36 +0000

    added calculations

--- Task 1.py
+++ Task 1.py
@@ -6,21 +6,36 @@
 #Purpose:
 #
 #The purpose of this program is to ask the user if they want to calculate the area of a circle, rectangle, or triangle.
+import math
+answer = input("Please enter if you would like to calculate the area of a triangle, " + \
+               "circle, or rectangle. Or if you would like to exit: ") #Get the user's choice 
 
-answer = input("Please enter if you would like to calculate the area of a triangle, circle, or rectangle. Or if you would like to exit: ") #Get the user's choice 
-
-while answer.strip().upper() == "TRIANGLE":
+if answer.strip().upper() == "TRIANGLE":
     triangleLength = float(input("Please enter the length of triangle in centimetres: "))
     
     triangleArea = (triangleLength * triangleLength) / 2
     trianglePerimeter = triangleLength * 3
     
-    print("The area of the triangle is: " + str(triangleArea) + "cm^2")
+    print("The area of the triangle is: " + str(triangleArea) + "cm^2.")
+    print("The perimeter of the triangle is " + str(trianglePerimeter) + "cm.")
     
-    break
+if answer.strip().upper() == "RECTANGLE":
+    rectangleLength = float(input("Please enter the length of the rectangle: "))
     
-while answer.strip().upper() == "RECTANGLE":
-    print(";asdf")
+    rectangleArea = rectangleLength * 2 
+    rectanglePerimeter = rectangleLength * 4 
     
-while answer.strip().upper() == "CIRCLE":
-    print("a;slkdfj")
+    print("The area of the rectangle is " + str(rectangleArea) + "cm^2.")
+    print("The perimeter of the rectangle is " + str(rectanglePerimeter) + "cm.")
+    
+if answer.strip().upper() == "CIRCLE":
+    circleRadius = float(input("Please enter the radius of the circle: "))
+    
+    circleArea = math.pi * (circleRadius * circleRadius)
+    circlePerimeter = 2 * math.pi * circleRadius
+    
+    print("The area of the circle is " + str(circleArea) + "cm^2.")
+    print("The perimeter of the circle is " + str(circlePerimeter) + "cm.")
+    
+if answer.strip().upper() == "EXIT":
+    print("OK, goodbye.")
