e3f53a53d9c5b1da5383bf551d2c82aa7ec9a287
DerekAB <123401143+DerekAB@users.noreply.github.com>
2023-02-03 00:53:17 +0000

    triangle

--- Task 1.py
+++ Task 1.py
@@ -7,13 +7,20 @@
 #
 #The purpose of this program is to ask the user if they want to calculate the area of a circle, rectangle, or triangle.
 
-answer = input("Please enter if you would like to calculate the area of a triangle, circle, or rectangle. Or if you would like to exit: ")
+answer = input("Please enter if you would like to calculate the area of a triangle, circle, or rectangle. Or if you would like to exit: ") #Get the user's choice 
 
 while answer.strip().upper() == "TRIANGLE":
+    triangleLength = float(input("Please enter the length of triangle in centimetres: "))
     
+    triangleArea = (triangleLength * triangleLength) / 2
+    trianglePerimeter = triangleLength * 3
+    
+    print("The area of the triangle is: " + str(triangleArea) + "cm^2")
+    
+    break
     
 while answer.strip().upper() == "RECTANGLE":
-    
+    print(";asdf")
     
 while answer.strip().upper() == "CIRCLE":
-    
+    print("a;slkdfj")
